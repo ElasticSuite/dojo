2dbff345200ece49b6a127ed734e30150e387551
Lyle Underwood <lyle.underwood@bkwld.com>
2013-02-12 21:58:31 -0800

    default parent to this.node if there is no dropParent

--- dnd/Container.js
+++ dnd/Container.js
@@ -67,7 +67,7 @@ var Container = declare("dojo.dnd.Container", Evented, {
 		if(!params){ params = {}; }
 		this.creator = params.creator || null;
 		this.skipForm = params.skipForm;
-		this.parent = params.dropParent && dom.byId(params.dropParent);
+		this.parent = (params.dropParent && dom.byId(params.dropParent)) || this.node;
 
 		// class-specific variables
 		this.map = {};
