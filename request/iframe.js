10900f9f665478a4b8849e496fa784d6c13d7123
Lyle Underwood <lyleunderwood@gmail.com>
2013-05-06 11:21:39 -0700

    why is this still broken, dojo?

--- request/iframe.js
+++ request/iframe.js
@@ -244,10 +244,10 @@ define([
 					}
 				}else{
 					if(!methodNode || !methodNode.value){
-						if(mthdNode){
+						if(typeof mthdNode !== 'undefined' && mthdNode){
 							mthdNode.value = options.method;
 						}else{
-							fn.setAttribute("method", options.method);
+							formNode.setAttribute("method", options.method);
 						}
 					}
 				}
